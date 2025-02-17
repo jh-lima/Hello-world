27401a61b799f0b7915a834e5412b4500a18e1d3
João Lima <joaohenriqueoliveiralima@hotmail.com>
2025-02-17 00:01:39 -0300

    Exercício - tabuada

---
+++ Tabuada.py
@@ -0,0 +1,15 @@
+def main():
+    num=int(input('Digite o número para o qual deseja saber a tabuada: '))
+    i=1
+    while i<= 10:
+        produto=num*i
+        print(f'{num} X {i} =', produto)
+        i+=1
+    print('Fim da tabuada')
+
+        
+        
+
+print('Fim da tabuada')
+  
+main()
